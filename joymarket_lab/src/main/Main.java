15a18a10594ee1e1296c8ddd26237630d12ed8b7
Melvin Jordan <melvin11278@gmail.com>
2025-12-05 23:39:09 +0700

    Edit: login view style and delete main constructor

--- joymarket_lab/src/main/Main.java
+++ joymarket_lab/src/main/Main.java
@@ -3,15 +3,14 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import view.LoginView;
 
-public class main {
+public class Main extends Application{
 
-	public main() {
 		// TODO Auto-generated constructor stub
 		public void start(Stage stage){
 			LoginView lv = new LoginView();
-			loginView.start(stage);
+			lv.start(stage);
 		}
-	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- joymarket_lab/src/view/LoginView.java
+++ joymarket_lab/src/view/LoginView.java
@@ -16,30 +16,46 @@ public class LoginView {
     public void start(Stage stage) {
 
         Label title = new Label("User Login");
-        title.setStyle("-fx-font-size: 20px; -fx-font-weight: bold;");
+        title.setStyle("-fx-font-size: 20px; -fx-font-weight: bold; -fx-text-fill: white;");
+        GridPane.setHalignment(title, HPos.CENTER);
 
         txtEmail = new TextField();
         txtEmail.setPromptText("Email");
+        txtEmail.setStyle("-fx-text-fill: white; -fx-control-inner-background: #2b2b2b");
+        txtEmail.setPrefWidth(200);
 
         txtPassword = new PasswordField();
         txtPassword.setPromptText("Password");
+        txtPassword.setStyle("-fx-text-fill: white; -fx-control-inner-background: #2b2b2b");
+        txtPassword.setPrefWidth(200);
 
         btnLogin = new Button("Login");
-        btnLogin.setStyle("-fx-background-color: #2196F3; -fx-text-fill: white;");
+        btnLogin.setStyle("-fx-background-color: #3B82F6; -fx-text-fill: white;");
 
         btnToRegister = new Button("Register");
+        btnToRegister.setStyle("-fx-background-color: white; -fx-text-fill: #3B82F6");
 
         GridPane layout = new GridPane();
+        layout.setStyle("-fx-background-color: #1e1e1e");
+        
         layout.setPadding(new Insets(20));
-        layout.setVgap(10);
+        layout.setVgap(12);
         layout.setHgap(10);
         layout.setAlignment(Pos.CENTER);
 
+        
+        Label lblEmail = new Label("Email:");
+        lblEmail.setStyle("-fx-text-fill: white;");      
+
+        Label lblPassword = new Label("Password:");
+        lblPassword.setStyle("-fx-text-fill: white;");    
+        
         layout.add(title, 0, 0, 2, 1);
-        layout.add(new Label("Email:"), 0, 1);
+        layout.add(lblEmail, 0, 1);
+        
         layout.add(txtEmail, 1, 1);
 
-        layout.add(new Label("Password:"), 0, 2);
+        layout.add(lblPassword, 0, 2);
         layout.add(txtPassword, 1, 2);
 
         layout.add(btnLogin, 1, 3);
