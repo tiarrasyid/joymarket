95b2f3765d25343f8e7d9a0e65b738bf66533736
Tiar Rasyid <80014064+tiarrasyid@users.noreply.github.com>
2025-12-06 12:42:47 +0700

    Add Customer class extending User with balance

---
+++ joymarket_lab/src/model/Customer.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Customer extends User {
+    private double userBalance;
+
+    public Customer(String userId, String userName, String userEmail, String userPassword, String userGender, String userAddress, String userPhone, double userBalance) {
+        super(userId, userName, userEmail, userPassword, userGender, userAddress, userPhone, "Customer");
+        this.userBalance = userBalance;
+    }
+
+    public double getUserBalance() { return userBalance; }
+    public void setUserBalance(double userBalance) { this.userBalance = userBalance; }
+}
